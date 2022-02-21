4754c6cd54f39c18e4ab73d6f094ff839922f908
lauterjung <miguelbusarello@hotmail.com>
2022-02-21 17:36:43 -0300

    Added kit attribute to Genotype class

--- classes/genotype.py
+++ classes/genotype.py
@@ -1,5 +1,6 @@
 class Genotype:
-    def __init__(self, locus: str, allele_1: str, allele_2: str):
+    def __init__(self, kit: str, locus: str, allele_1: str, allele_2: str):
+        self.kit = kit
         self.locus = locus
         self.allele_1 = allele_1
         self.allele_2 = allele_2
