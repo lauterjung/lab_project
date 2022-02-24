5fb1352a7194cc4126c4def20911ed7a52bb7e09
Lauterjung <miguelbusarello@hotmail.com>
2022-02-24 13:23:15 -0300

    type_of_case WIP

--- model/lab_case.py
+++ model/lab_case.py
@@ -9,9 +9,35 @@ class LabCase:
         self.name = name
         self.subjects = subjects
 
-    def type_of_case():
+    def type_of_case(self):
         pass
-    # return type_of_case
-    # DUO
-    # TRIO
-    # COMPLEX
+        # for subject in self.subjects:
+        #     if subject.type == "F": # como acessar direto os subject types pra verificar se "F in names"?
+        #         continue
+        #     else:
+        #         # Raise error: F not found
+        #         break
+        # if len(self.subjects) > x:
+ 
+        # return type_of_case
+        # DUO
+        # TRIO
+        # COMPLEX
+        
+# old code (R) for basis
+        
+# tipo.caso <- ""
+#   if(length(unique(dados$nomenclatura)) == 3 & # se não tiver essa linha, duo entra como trio
+# 	 all(unique(dados$nomenclatura) %in% c("M", "F", "SP"))){
+#     tipo.caso <- "Trio"
+#   } 
+  
+#   if(length(unique(dados$nomenclatura)) == 2 &
+# 	(all(unique(dados$nomenclatura) %in% c("F", "SP"))|
+#   all(unique(dados$nomenclatura) %in% c("F", "SM")))){
+#     tipo.caso <- "Duo"
+#   } 
+
+#   if(tipo.caso != "Trio" & tipo.caso != "Duo"){ 
+#     tipo.caso <- "Complexo"
+#   } 
