f6bd3b1b397bd3934d57e1fafa742bd4b6d992c2
lauterjung <miguelbusarello@hotmail.com>
2022-03-07 18:34:36 -0300

    WIP

--- tests/case_processing_service_test.py
+++ tests/case_processing_service_test.py
@@ -4,95 +4,21 @@ from model.genotype import Genotype
 from model.lab_case import LabCase
 
 from model.subject import Subject
-from tests.utils.available_subjects import AvailableSubjects
+from tests.utils.test_setup import TestSetup
 
 class CaseProcessingServiceTest(unittest.TestCase):
     def setUp(self):
-        self.all_subjects = AvailableSubjects()
+        self.test_setup = TestSetup()
 
     def test_amelogenin_swap(self):
         case_processing_service = CaseProcessingService
-        
-        male_genotype = Genotype("VE", "Amel", "X", "Y")
-        female_genotype = Genotype("VE", "Amel", "X", "X")
-
-        self.all_subjects.child.genetic_profile.append(male_genotype)
-
-        self.all_subjects.child.genetic_profile.append(male_genotype)
-        self.all_subjects.child_2.genetic_profile.append(male_genotype)
-        self.all_subjects.biologic_child_1.genetic_profile.append(male_genotype)
-        self.all_subjects.biologic_child_2.genetic_profile.append(male_genotype)
-        self.all_subjects.auxiliary_child_1_1.genetic_profile.append(male_genotype)
-        self.all_subjects.auxiliary_child_1_2.genetic_profile.append(male_genotype)
-        self.all_subjects.auxiliary_child_2_1.genetic_profile.append(male_genotype)
-        self.all_subjects.auxiliary_child_2_2.genetic_profile.append(male_genotype)
-        self.all_subjects.maternal_uncle_1.genetic_profile.append(male_genotype)
-        self.all_subjects.maternal_uncle_2.genetic_profile.append(male_genotype)
-        self.all_subjects.alledged_maternal_uncle_1.genetic_profile.append(male_genotype)
-        self.all_subjects.alledged_maternal_uncle_2.genetic_profile.append(male_genotype)
-        self.all_subjects.paternal_uncle_1.genetic_profile.append(male_genotype)
-        self.all_subjects.paternal_uncle_2.genetic_profile.append(male_genotype)
-        self.all_subjects.alledged_paternal_uncle_1.genetic_profile.append(male_genotype)
-        self.all_subjects.alledged_paternal_uncle_2.genetic_profile.append(male_genotype)
-        self.all_subjects.another_1.genetic_profile.append(male_genotype)
-        self.all_subjects.another_2.genetic_profile.append(male_genotype)
-
-        self.all_subjects.mother.genetic_profile.append(female_genotype)
-        self.all_subjects.alledged_father.genetic_profile.append(male_genotype)
-        self.all_subjects.alledged_mother.genetic_profile.append(female_genotype)
-        self.all_subjects.father.genetic_profile.append(male_genotype)
-        self.all_subjects.auxiliary_mother_1.genetic_profile.append(female_genotype)
-        self.all_subjects.auxiliary_mother_2.genetic_profile.append(female_genotype)
-        self.all_subjects.auxiliary_father_1.genetic_profile.append(male_genotype)
-        self.all_subjects.auxiliary_father_2.genetic_profile.append(male_genotype)
-        self.all_subjects.maternal_grandmother.genetic_profile.append(female_genotype)
-        self.all_subjects.alledged_maternal_grandmother.genetic_profile.append(female_genotype)
-        self.all_subjects.maternal_grandfather.genetic_profile.append(male_genotype)
-        self.all_subjects.alledged_maternal_grandfather.genetic_profile.append(male_genotype)
-        self.all_subjects.paternal_grandmother.genetic_profile.append(female_genotype)
-        self.all_subjects.alledged_paternal_grandmother.genetic_profile.append(female_genotype)
-        self.all_subjects.paternal_grandfather.genetic_profile.append(male_genotype)
-        self.all_subjects.alledged_paternal_grandfather.genetic_profile.append(male_genotype)
-
-        self.all_subjects.swapped_mother.genetic_profile.append(male_genotype)
-        self.all_subjects.swapped_alledged_father.genetic_profile.append(female_genotype)
-        self.all_subjects.swapped_alledged_mother.genetic_profile.append(male_genotype)
-        self.all_subjects.swapped_father.genetic_profile.append(female_genotype)
-        self.all_subjects.swapped_auxiliary_mother_1.genetic_profile.append(male_genotype)
-        self.all_subjects.swapped_auxiliary_mother_2.genetic_profile.append(male_genotype)
-        self.all_subjects.swapped_auxiliary_father_1.genetic_profile.append(female_genotype)
-        self.all_subjects.swapped_auxiliary_father_2.genetic_profile.append(female_genotype)
-        self.all_subjects.swapped_maternal_grandmother.genetic_profile.append(male_genotype)
-        self.all_subjects.swapped_alledged_maternal_grandmother.genetic_profile.append(male_genotype)
-        self.all_subjects.swapped_maternal_grandfather.genetic_profile.append(female_genotype)
-        self.all_subjects.swapped_alledged_maternal_grandfather.genetic_profile.append(female_genotype)
-        self.all_subjects.swapped_paternal_grandmother.genetic_profile.append(male_genotype)
-        self.all_subjects.swapped_alledged_paternal_grandmother.genetic_profile.append(male_genotype)
-        self.all_subjects.swapped_paternal_grandfather.genetic_profile.append(female_genotype)
-        self.all_subjects.swapped_alledged_paternal_grandfather.genetic_profile.append(female_genotype)
-
-        correct_case_trio = LabCase(self.all_subjects.case_name)
-        correct_case_trio.subjects.append(self.all_subjects.mother, self.all_subjects.child, self.all_subjects.alledged_father)
-        swapped_case_trio = LabCase(self.all_subjects.case_name)
-        swapped_case_trio.subjects.append(self.all_subjects.swapped_mother, self.all_subjects.child, self.all_subjects.alledged_father)
-        
-        correct_case_duo = LabCase(self.all_subjects.case_name)
-        correct_case_duo.subjects.append(self.all_subjects.child, self.all_subjects.alledged_father)
-        swapped_case_duo = LabCase(self.all_subjects.case_name)
-        swapped_case_duo.subjects.append(self.all_subjects.child, self.all_subjects.swapped_alledged_father)
-
-        no_gender_case_complex = LabCase(self.all_subjects.case_name)
-        no_gender_case_complex.subjects.append(self.all_subjects.child, self.all_subjects.paternal_uncle_1, self.all_subjects.maternal_uncle_1, self.all_subjects.another_1)
 
         # check swapp for the whole case or individually?
-        self.assertTrue(case_processing_service.check_amelogenin_swap(swapped_case_trio))
-        self.assertFalse(case_processing_service.check_amelogenin_swap(correct_case_trio))
-        self.assertTrue(case_processing_service.check_amelogenin_swap(swapped_case_duo))
-        self.assertFalse(case_processing_service.check_amelogenin_swap(correct_case_duo))
-        self.assertFalse(case_processing_service.check_amelogenin_swap(no_gender_case_complex))
+        self.assertTrue(case_processing_service.check_amelogenin_swap(self.test_setup.swapped_case_trio))
+        self.assertFalse(case_processing_service.check_amelogenin_swap(self.test_setup.correct_case_trio))
+        self.assertTrue(case_processing_service.check_amelogenin_swap(self.test_setup.swapped_case_duo))
+        self.assertFalse(case_processing_service.check_amelogenin_swap(self.test_setup.correct_case_duo))
+        self.assertFalse(case_processing_service.check_amelogenin_swap(self.test_setup.no_gender_case_complex))
     
     def test_set_case_subtype(self):
         pass
-
-    def tearDown(self):
-        self.all_subjects.dispose()
