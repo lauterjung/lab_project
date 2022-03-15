5dea5e1a1d291bbb4d7a2413188b8a88775deb88
lauterjung <miguelbusarello@hotmail.com>
2022-03-15 15:42:09 -0300

    Added final to EXCLUDE_FROM_CALC

--- model/genotype.py
+++ model/genotype.py
@@ -1,3 +1,6 @@
+from typing import Final
+
+
 class Genotype:
     exclude_from_calculations: bool
 
@@ -9,7 +12,7 @@ class Genotype:
         self.exclude_from_calculations = self.__set_exclude_from_calc()
     
     def __set_exclude_from_calc(self) -> bool:
-        EXCLUDE_FROM_CALC = ["Amel", "Yindel", "DYS391", "DYS576", "DYS570"]
+        EXCLUDE_FROM_CALC: Final[str] = ["Amel", "Yindel", "DYS391", "DYS576", "DYS570"]
         
         if self.locus in EXCLUDE_FROM_CALC:
             return True
