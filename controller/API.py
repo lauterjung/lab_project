5367fe95dcb38e57d83db0a89bea7aea46dd086e
lauterjung <miguelbusarello@hotmail.com>
2022-03-15 16:19:56 -0300

    API implementation plan

---
+++ controller/API.py
@@ -0,0 +1,12 @@
+# inform path to .csv
+# read .csv containing all cases
+# register cases to DB from single csv file (or for each case .csv after split)
+# verify case type
+# verify swaps and case subtype
+
+# TODO: calculations
+# generate summary table (Case, Type, Log)
+# TODO: better log (which locus has inconsistencies?)
+# TODO: if exclusion, generate request to secretary (.txt) and print
+
+# TODO: split .csv for each case, or generate .csv from DB
