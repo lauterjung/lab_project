18838c042b611ee5940aac62d53ee75f7e37c4ae
lauterjung <miguelbusarello@hotmail.com>
2022-04-26 13:04:49 -0300

    Implementation of inconsistencies and subtype_of_case methods

--- view/user_interface.py
+++ view/user_interface.py
@@ -2,7 +2,6 @@ from contextlib import redirect_stdout
 from controller.case_processing_service import CaseProcessingService
 from controller.database import LabCaseDB
 from controller.lab_case_controller import LabCaseController
-from model.lab_case import LabCaseType
 
 db = LabCaseDB()
 controller = LabCaseController(db)
@@ -30,38 +29,9 @@ for case in db.lab_cases:
     case_processing.populate_lab_case(controller, case)
     result_table_1.append(case_processing.case_to_result_table(case))
     
-    if case.type_of_case == LabCaseType.trio:
-        vector = case_processing.OLD_check_inconcistencies_trio(case)
-        results_swap = ""
-        results_mutation_C_AF = ""
-        results_mutation_M_C = ""
-
-        if len(case.mother_x_alledged_father) <= 3 or len(case.mother_x_child) > 3:
-            results_swap = "TROCA"
-        if 0 < len(case.mother_x_child) <= 3:
-            results_mutation_M_C = "Mutação entre M e F no(s) loco(s): " + " ".join(case.mother_x_child) + "."
-        if 0 < len(case.child_x_alledged_father) <= 3:
-            results_mutation_C_AF = "Mutação entre F e SP no(s) loco(s): " + ", ".join(case.child_x_alledged_father) + "."
-        inconsistency_list = " ".join([results_swap, results_mutation_M_C, results_mutation_C_AF]).strip()
-    else:
-        vector = []
-        inconsistency_list = []
-    
-    result_table_2.append((case.name, case.type_of_case.name, case.amelogenin_swap, case.inconsistencies_vector, inconsistency_list))
-    result_table_2
+result_table_1
 
 # with open('output_1.txt', 'w') as f:
 #     with redirect_stdout(f):
 #         for line in result_table_1:
 #             print(line, sep='\n')
-
-# with open('output_2.txt', 'w') as f:
-#     with redirect_stdout(f):
-#         for line in result_table_2:
-#             print(line, sep='\n')
-
-# for case in db.lab_cases:
-#     for subject in case.subjects:
-#         print(subject.name)
-#         for genotype in subject.genetic_profile:
-#             print(genotype.locus + " " + genotype.allele_1 + " " + genotype.allele_2)
